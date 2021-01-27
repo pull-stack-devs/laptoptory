bef7e66365cbd03840e2d11b26f6d7d694b47320
Abd Alrhman <abdalrhman@Abds-MacBook-Pro.local>
2021-01-27 12:23:16 +0200

    url

--- pool.js
+++ pool.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const Pool = require('pg').Pool;
-require('dotenv').config();
-let url = 'postgres://furjbrlrrrfcew:37a8a307e807cc75a06bf409ea4b3a5c9f78b5af052f2e6bbd972294cdc25e86@ec2-34-248-165-3.eu-west-1.compute.amazonaws.com:5432/dcli37iogr3ev1'
+
+let url = process.env.DATABASE_url;
 const pool = new Pool({
   url
 });
