889f20d497e0a9c41ae09ed369dbbbf880a38e59
HusamAjour <husamaj278@gmail.com>
2021-02-25 18:28:52 +0200

    fix async awaut issue

--- middleware/500.js
+++ middleware/500.js
@@ -1,5 +1,6 @@
 module.exports = (err, req, res, next) => {
     console.log('inside Err Handler', err);
+    console.log(err);
     res.status(500); // internal server error status code
     res.json({ error: 'We have an issue we will fix it soon!' });
   };
