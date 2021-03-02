0f13a77463773b7eafd6e1037e947143f42fce32
HusamAjour <husamaj278@gmail.com>
2021-03-02 13:05:06 +0200

    socket io"

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const express = require('express');
+const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
+const io = socketIO(app);
 app.use(express.json());
 app.use(cors());
 const laptopRoutes = require('./lib/laptop-routes');
@@ -15,6 +17,8 @@ const PORT = process.env.PORT;
 const router = require('./lib/router');
 const notFoundHandler = require('./middleware/404');
 const errorHandler = require('./middleware/500');
+const bearerMiddleware = require('./middleware/bearer-auth');
+const authorizeMid = require('./middleware/authorize');
 
 const mongoose = require('mongoose');
 
@@ -40,6 +44,42 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
-app.listen(PORT, () => {
+
+const queue = {
+  notifications: {}
+};
+
+app.get(
+  '/notifications/all',
+  bearerMiddleware,
+  authorizeMid('approve'),
+  async (req, res) => {
+    try {
+      // let rows = await users.read();
+      res.status(200).json(queue);
+    } catch (err) {
+      errorHandler(err);
+    }
+  }
+);
+
+io.on('connection', socket=> {
+  console.log("socket: random");
+  socket.on('signup', payload=> {
+      console.log("New User signup!");
+      let id = Math.random();
+      queue.notifications[id] = payload;
+      // socket.broadcast.emit('New user', {payload})
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+
+isten(PORT, () => {
   console.log(`App running on port ${PORT}.`);
 });
+
